ec9cc15657ca95a6e443b383b3bd6b52da3d71fa
Imon-Ovbude <imonhimio@gmail.com>
2021-12-08 12:19:38 -0800

    setup nav component and styles

--- hubble/src/components/Header.js
+++ hubble/src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { StyledHeader } from './styles/Header.styled';
+import { StyledHeader, Nav, Logo } from './styles/Header.styled';
+import { Container } from './styles/Container.styled';
+import { Button } from './styles/Button.styled';
 
 function Header() {
   return (
     <StyledHeader>
-      <h1>Hubble</h1>
+      <Container>
+        <Nav>
+          <Logo src='./images/logo.svg' alt='logo'></Logo>
+          <Button>Try It Free</Button>
+        </Nav>
+      </Container>
     </StyledHeader>
   );
 }
